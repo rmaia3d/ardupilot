4a7ab3fdd664cfd6fdd67b018ccb592476a84cdf
rmaia <9812730+rmaia3d@users.noreply.github.com>
2024-01-03 18:09:44 -0300

    Plane: Add extended flight modes (increase total to 12)
    Adds extra 6 flight modes, enabled via a parameter, which brings the total of selectable flight modes to 12. Only available in Plane.

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -405,6 +405,7 @@ private:
 
     bool read_3pos_switch(AuxSwitchPos &ret) const WARN_IF_UNUSED;
     bool read_6pos_switch(int8_t& position) WARN_IF_UNUSED;
+    bool read_extended_mode_switch(int8_t &position) WARN_IF_UNUSED;
 
     // Structure used to detect and debounce switch changes
     struct {
@@ -415,7 +416,7 @@ private:
     } switch_state;
 
     void reset_mode_switch();
-    void read_mode_switch();
+    void read_mode_switch(bool extended = false);
     bool debounce_completed(int8_t position);
     // returns true if the first time we successfully read the
     // channel's three-position-switch position we should record that
@@ -504,7 +505,7 @@ public:
 
     // mode switch handling
     void reset_mode_switch();
-    virtual void read_mode_switch();
+    virtual void read_mode_switch();    
 
     virtual bool in_rc_failsafe() const { return true; };
     virtual bool has_valid_input() const { return false; };
@@ -591,6 +592,9 @@ public:
 
     // flight_mode_channel_number must be overridden in vehicle specific code
     virtual int8_t flight_mode_channel_number() const = 0;
+
+    // is_extended_flight_modes_enabled must be overridden in vehicle specific code
+    virtual bool is_extended_flight_modes_enabled() const = 0;
 
     // set and get calibrating flag, stops arming if true
     void calibrating(bool b) { gcs_is_calibrating = b; }
